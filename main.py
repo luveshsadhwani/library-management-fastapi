3fa7faa27019691bc92baebdbe482e06d83dbeaf
Ali™ <76595455+Ali-TM-original@users.noreply.github.com>
2021-05-29 17:32:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ app = FastAPI()
 
 app.include_router(route.router)
 # app.add_middleware(BaseHTTPMiddleware, dispatch=auth_check)
+
+
+"""
+  TO run the api in terminal change directory to where main.py is located
+  now type uvicorn main:app --reload    where main is the name of the file i.s(main.py in full form ) and app is the instance of the fastapi
+  indicated by app = FastApi()
+  
+  the app will run default on port 8000
+  
+  to include routes line 10 has been used
+  to include middleware line 4 and 2 and 8 has been added but i've commented it to test without the middleware otherwise we will have to send headers with
+  each http request
+
+"""
