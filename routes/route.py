971dab3cea0f7fb752e2b3af894425a3c20f347e
Ali™ <76595455+Ali-TM-original@users.noreply.github.com>
2021-05-30 14:28:43 +0500

    Formatted Book data exactly same as Fake json

--- routes/route.py
+++ routes/route.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from .db import AuthDb
 
+# from fastapi.responses import UJSONResponse
 
 
 router = APIRouter()
@@ -19,8 +20,8 @@ async def home():
 
 @router.get("/data")
 async def data():
-    data = await AuthDb.return_data()
-    return [{"data": data}]
+    info = await AuthDb.return_data()
+    return info
 
 
 @router.get("/login")
@@ -28,16 +29,10 @@ async def read_item(username: str = None, password: str = None):
     if username is None and password is None:
         return {False}
     else:
-        data = await AuthDb.login_check(username, password)
-        return {data}
+        info = await AuthDb.login_check(username, password)
+        return {info}
 
 
-@router.get("/test")
-async def test():
-    data = await AuthDb.check_token("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2siOiJvZXhvb2R4c2hzIn0.x8ZEh...")
-    print(data)
-    if data is None:
-        return data
-    else:
-        print(data)
-        return {True}
+@router.post("/entry")
+async def post():
+    return True
